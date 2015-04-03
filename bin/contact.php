c96e18d0ac53a583800a3d45c2bbfb6eb440f6c6
Piergiorgio Faraglia <p.faraglia@gmail.com>
2015-04-03 13:12:42 +0100

    Update documentation

---
+++ bin/contact.php
@@ -0,0 +1,69 @@
+<?php
+
+//Retrieve form data. 
+//GET - user submitted data using AJAX
+//POST - in case user does not support javascript, we'll use POST instead
+$name = ($_GET['name']) ? $_GET['name'] : $_POST['name'];
+$email = ($_GET['email']) ?$_GET['email'] : $_POST['email'];
+$comment = ($_GET['comment']) ?$_GET['comment'] : $_POST['comment'];
+
+//flag to indicate which method it uses. If POST set it to 1
+
+if ($_POST) $post=1;
+
+$errors = null;
+
+//Simple server side validation for POST data, of course, you should validate the email
+if (!$name) $errors[count($errors)] = 'Please enter your name.';
+if (!$email) $errors[count($errors)] = 'Please enter your email.'; 
+if (!$comment) $errors[count($errors)] = 'Please enter your comment.'; 
+
+//if the errors array is empty, send the mail
+if (!$errors) {
+
+	//recipient - replace your email here
+	$to = 'piefa703@gmail.com';
+	//sender - from the form
+	$from = $name . ' <' . $email . '>';
+	
+	//subject and the html message
+	$subject = '[piergiorgiofaraglia.it] Richiesta di contatto da ' . $name;
+	$message = sprintf("Nome:%s<br/><br/>Email:%s<br/><br/>Messaggio:%s<br/>", htmlentities(utf8_decode($name)), $email, nl2br(htmlentities(utf8_decode($comment))));
+
+	//send the mail
+	$result = sendmail($to, $subject, $message, $from);
+	
+	//if POST was used, display the message straight away
+	if ($_POST) {
+		if ($result) echo 'Grazie! Il tuo messaggio è stato correttamente inviato.';
+		else echo 'Spiacenti, errore inaspettao. Riprovare più tardi.';
+		
+	//else if GET was used, return the boolean value so that 
+	//ajax script can react accordingly
+	//1 means success, 0 means failed
+	} else {
+		echo $result;	
+	}
+
+//if the errors array has values
+} else {
+	//display the errors message
+	for ($i=0; $i<count($errors); $i++) echo $errors[$i] . '<br/>';
+	echo '<a href="index.html">Back</a>';
+	exit;
+}
+
+
+//Simple mail function with HTML header
+function sendmail($to, $subject, $message, $from) {
+	$headers = "MIME-Version: 1.0" . "\r\n";
+	$headers .= "Content-type:text/html;charset=iso-8859-1" . "\r\n";
+	$headers .= 'From: ' . $from . "\r\n";
+	
+	$result = mail($to,$subject,$message,$headers);
+	
+	if ($result) return 1;
+	else return 0;
+}
+
+?>
