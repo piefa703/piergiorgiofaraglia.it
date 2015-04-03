11ded1d6d28a2fc5c9ea617ac5e9617b22bdd61d
Piergiorgio Faraglia <p.faraglia@gmail.com>
2015-04-03 13:56:37 +0200

    removes old site

--- theme/js/twitterusername.js
+++
@@ -1,8 +0,0 @@
-jQuery(function($){
-        $(".tweet").tweet({
-            username: "xm3ron",
-            avatar_size: 50,
-            count: 1,
-            loading_text: "loading tweets..."
-        });
-    });
